1fc41d3b468eb3767564f7b66db870f1a0ace5d2
xuanhui <987863597@qq.com>
2022-01-17 15:15:55 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.less';
+import { Button, Space } from 'antd-mobile'
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+      <Button color='primary'>Primary</Button>
+      <Button color='success'>Success</Button>
+    </div>
+  );
+}
