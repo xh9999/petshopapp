4fc8f6a59e745a86adee83ee450c88533a3da6d3
KIkuwei <879952062@qq.com>
2022-01-18 20:40:59 +0800

    tijiao

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,14 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
-  fastRefresh: {}
+  routes: [{ path: '/myOrder', component: '@/pages/myOrder/index' }],
+  fastRefresh: {},
+  mock: {},
+  proxy: {
+    '/api': {
+      target: 'http://110.42.190.78:8888',
+      changeOrigin: true,
+      // 'pathRewrite': { '^/api': '' },
+    },
+  },
 });
