df965c3b1cc71f9045cfdb4b475939c1a7019da4
lhn129908 <1299085695@qq.com>
2022-01-20 01:48:12 +0800

    上传用户中心

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,21 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/index' },
+    // 我的页面
+    { path: '/my', component: '@/pages/my' },
+    // 个人资料
+    { path: '/person', component: '@/pages/my/person' },
+    // 我的订单
+    { path: '/order', component: '@/pages/my/order' },
+    // 账单地址
+    { path: '/address', component: '@/pages/my/address' },
   ],
-  fastRefresh: {}
+  fastRefresh: {},
+  proxy: {
+    '/api': {
+      target: 'http://110.42.190.78:8888',
+      changeOrigin: true,
+      // 'pathRewrite': { '^/api': '' },
+    },
+  },
 });
---
+++ src/pages/my/order.tsx
@@ -0,0 +1,9 @@
+import styles from './order.less';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <h1>我是订单页面</h1>
+    </div>
+  );
+}
